ad7a72d238d263e887949aa4cf4b534c543346d8
Kristine Stray <kstray@stud.ntnu.no>
2021-11-15 22:52:54 +0100

    Add custom font (weather icons)

--- src/display_ssd16xx.c
+++ src/display_ssd16xx.c
@@ -2,13 +2,21 @@
 #include <device.h>
 #include <drivers/display.h>
 #include <display/cfb.h>
-#include <logging/log.h>
 #include <stdio.h>
+
+#include "cfb_font_weather.h"
 
 
 #if DT_NODE_HAS_STATUS(DT_INST(0, solomon_ssd16xxfb), okay)
 #define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, solomon_ssd16xxfb))
 #endif
+
+// typedef enum weather{
+//     SUNNY,
+//     CLOUDY,
+//     RAINY,
+//     SNOWY
+// };
 
 
 void display_init() {
@@ -18,9 +26,6 @@ void display_init() {
     uint16_t rows;
     uint8_t ppt, font_width, font_height;
 
-    // uint8_t *buf;
-    // struct display_buffer_descriptor buf_desc;
-    // size_t buf_size = 0;
 
     dev = device_get_binding(DISPLAY_DEV_NAME);
     if (dev == NULL) {
@@ -40,56 +45,59 @@ void display_init() {
         printk("CFB init error\n");
         return;
     }
-    //cfb_framebuffer_clear(dev, false);
-    //cfb_framebuffer_finalize(dev);
-
-    //k_sleep(K_SECONDS(10));
-
-    rows = cfb_get_display_parameter(dev, CFB_DISPLAY_ROWS);
-    ppt = cfb_get_display_parameter(dev, CFB_DISPLAY_PPT);
 
     for (int i = 0; i < 42; i++) {
         if (cfb_get_font_size(dev, i, &font_width, &font_height)) {
             break;
         }
-        cfb_framebuffer_set_font(dev, i);
         printk("font width %d, font height %d\n", font_width, font_height);
     }
 
-    //cfb_framebuffer_set_font(dev, 0);
 
-    printk("x_res %d, y_res %d, ppt %d, rows %d, cols %d\n",
-	       cfb_get_display_parameter(dev, CFB_DISPLAY_WIDTH),
-	       cfb_get_display_parameter(dev, CFB_DISPLAY_HEIGH),
-	       ppt,
-	       rows,
-	       cfb_get_display_parameter(dev, CFB_DISPLAY_COLS));
-    
     cfb_framebuffer_clear(dev, false);
-    for (int i = 0; i < 3; i++) {
-        if (cfb_print(dev, "<3<3<3<3", 32, i * font_height)) {
-            printk("Failed to print string\n");
-            //continue;
-        }
+
+
+    err = cfb_framebuffer_set_font(dev, 2);
+    if (err) {
+        printk("Could not set font, err %d\n", err);
     }
+
+    err = cfb_print(dev, "Trondheim", 8, 8);
+    if (err) {
+        printk("Could not display string, err %d\n", err);
+    }
+
+    err = cfb_framebuffer_set_font(dev, 1);
+    if (err) {
+        printk("Could not set font, err %d\n", err);
+    }
+
+    err = cfb_print(dev, "Today", 8, 48);
+    if (err) {
+        printk("Could not display string, err %d\n", err);
+    }
+
+    err = cfb_print(dev, "Tomorrow", 8, 72);
+    if (err) {
+        printk("Could not display string, err %d\n", err);
+    }
+
+    err = cfb_framebuffer_set_font(dev, 0);
+    if (err) {
+        printk("Could not set font, err %d\n", err);
+    }
+
+    err = cfb_print(dev, "3", 200, 48);
+    if (err) {
+        printk("Could not display custom font, err %d\n", err);
+    }
+
+    err = cfb_print(dev, "4", 200, 72);
+    if (err) {
+        printk("Could not display custom font, err %d\n", err);
+    }
+
     cfb_framebuffer_finalize(dev);
-
-
-
-    // buf_size = 64;
-    // buf = k_malloc(buf_size);
-    // if (buf == NULL) {
-    //     printk("Could not allocate buffer memory\n");
-    //     return;
-    // }
-
-    // buf_desc.buf_size = buf_size;
-    // buf_desc.pitch = 8;
-    // buf_desc.width = 8;
-    // buf_desc.height = 8;
-
-    //(void)memset(buf, 0b01010101, buf_size);
-    //display_write(dev, 64, 64, &buf_desc, buf);
 
     return;
 }
